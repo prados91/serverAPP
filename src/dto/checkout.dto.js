e3628e179481cf3dcc606aaa02f928d5d3750944
Augusto <augustoprados91@gmail.com>
2024-05-28 22:22:48 -0300

    fix

--- src/dto/checkout.dto.js
+++ src/dto/checkout.dto.js
@@ -4,7 +4,6 @@ class CheckoutDTO {
             product_data: { name: data.product_id.title },
             currency: "usd",
             unit_amount: data.product_id.price * 100,
-            //centavos de dolar!!!
         };
         this.quantity = data.quantity;
     }
