ad7679520219b3d6ca43e741a5b86cfa1c77514f
Augusto <augustoprados91@gmail.com>
2024-05-31 21:06:30 -0300

    stripe redirect

--- src/repositories/payments.rep.js
+++ src/repositories/payments.rep.js
@@ -1,30 +1,3 @@
-/*import Stripe from "stripe";
-import CheckoutProduct from "../data/dto/checkout.dto.js";
-import cartsManager from "../data/mongo/CartsManager.mongo.js";
-//IMPLEMENTAR dao!!! ACA NO FUE IMPLEMENTADO
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-const checkoutRepository = async (filter) => {
-    try {
-        let productsOnCart = await cartsManager.read(filter);
-        productsOnCart = productsOnCart.map((each) => new CheckoutProduct(each));
-        console.log(productsOnCart);
-        const line_items = productsOnCart;
-        const mode = "payment";
-        const success_url = "http://localhost:8080/thanks.html";
-        const intent = await stripe.checkout.sessions.create({
-            line_items,
-            mode,
-            success_url,
-        });
-        return intent;
-    } catch (error) {
-        throw error;
-    }
-};
-
-export default checkoutRepository;*/
 import Stripe from "stripe";
 import env from "../utils/env.utils.js";
 import dao from "../data/index.factory.js";
@@ -43,8 +16,9 @@ class PaymentsRep {
         productsOnCart = productsOnCart.map((each) => new CheckoutDTO(each));
         const line_items = productsOnCart;
         const mode = "payment";
-        //const success_url = "http://localhost:5173/thanks.html";
-        const success_url = "https://node-55655-react.vercel.app/thanks";
+        //const success_url = "http://localhost:5173/thanks";
+        //const success_url = "https://node-55655-react.vercel.app/thanks";
+        const success_url = "http://coderbasketstore.netlify.app/index.html"
         const intent = await stripe.checkout.sessions.create({
             line_items,
             mode,
